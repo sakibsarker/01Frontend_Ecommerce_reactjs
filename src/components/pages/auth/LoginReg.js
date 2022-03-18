c49a5bc1883501060d2a5354e9a28aa3c552709e
sakibsarker <sakib15-3824@diu.edu.bd>
2022-03-18 17:16:15 +0600

    login tab work for login and reg

--- src/components/pages/auth/LoginReg.js
+++ src/components/pages/auth/LoginReg.js
@@ -1,6 +1,24 @@
 import { Grid,Card,Typography,Tabs,Tab,Box } from '@mui/material';
+import { useState } from 'react';
 import Pic1 from '../../../images/pic1'
+
+const TabPanel=(props)=>{
+    const{children,value,index}=props;
+    return(
+        <div role='tabpanel' hidden={value !==index}>
+            {
+                value===index&&(
+                    <Box>{children}</Box>
+                )
+            }
+        </div>
+    )
+}
 const LoginReg=()=>{
+    const [value,setValue]=useState(0);
+    const handleChange=(event,newValue)=>{
+        setValue(newValue);
+    }
     return<>
     <Grid container sx={{height:'80vh'}}>
         <Grid item Lg={7} sm={5} sx={{
@@ -15,10 +33,12 @@ const LoginReg=()=>{
         <Card sx={{width:'100%',height:'100%'}}>
             <Box>
                 <Box sx={{borderBottom:1,borderColor:'divider'}}>
-                    <Tabs textColor='secondary' indicatorColor='secondary'>
+                    <Tabs value={value} textColor='secondary' indicatorColor='secondary' onChange={handleChange}>
                     <Tab label='Login' sx={{textTransform: 'none',fontWeight: 'Bold'}} ></Tab>
                     <Tab label='Registration' sx={{textTransform:'none',fontWeight:'Bold'}} ></Tab>
                     </Tabs>
+                    <TabPanel value={value} index={0}>User Login</TabPanel>
+                    <TabPanel value={value} index={1}>Registration</TabPanel>
                 </Box>
             </Box>
         </Card>
